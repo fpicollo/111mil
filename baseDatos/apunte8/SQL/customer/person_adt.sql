7de9dc0274344ae9448c36e7f43e66523ba2d8f4
German Basisty <basisty.german@gmail.com>
2017-12-02 11:17:38 -0300

    Actualización

--- baseDatos/apunte8/SQL/customer/person_adt.sql
+++ baseDatos/apunte8/SQL/customer/person_adt.sql
@@ -3,3 +3,50 @@ CREATE TABLE customer.person(
 	cuit                text PRIMARY KEY,
 	iibb                text UNIQUE
 );
+
+
+CREATE OR REPLACE FUNCTION customer.person(
+	p_cuit              text,
+	p_iibb              text
+) RETURNS boolean AS $$
+DECLARE 
+	cuit_count          integer;
+	iibb_count          integer;
+	
+BEGIN
+	cuit_count := count(1) FROM customer.person WHERE cuit = p_cuit;
+	iibb_count := count(1) FROM customer.person WHERE iibb = p_iibb;
+	
+	IF cuit_count = 0 AND iibb_count = 0
+	THEN 
+		INSERT INTO customer.person VALUES(p_cuit, p_iibb);
+		RETURN TRUE;
+	ELSE 
+		RAISE WARNING 'CUIT O IIBB YA EXISTEN EN LA BASE DE DATOS';
+		RETURN FALSE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql
+SET search_path FROM CURRENT;
+
+
+CREATE OR REPLACE FUNCTION customer.person(
+	p_cuit              text
+) RETURNS boolean AS $$
+DECLARE 
+	cuit_count          integer;
+	
+BEGIN
+	cuit_count := count(1) FROM customer.person WHERE cuit = p_cuit;
+	
+	IF cuit_count = 0
+	THEN 
+		INSERT INTO customer.person(cuit) VALUES(p_cuit);
+		RETURN TRUE;
+	ELSE 
+		RAISE WARNING 'CUIT YA EXISTEN EN LA BASE DE DATOS';
+		RETURN FALSE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql
+SET search_path FROM CURRENT;
