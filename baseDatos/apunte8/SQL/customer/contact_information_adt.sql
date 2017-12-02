da36283051f3f4440c963db4298b39ed367f941a
German Basisty <basisty.german@gmail.com>
2017-12-02 12:18:35 -0300

    Actualización

--- baseDatos/apunte8/SQL/customer/contact_information_adt.sql
+++ baseDatos/apunte8/SQL/customer/contact_information_adt.sql
@@ -6,3 +6,32 @@ CREATE TABLE customer.contact_information (
 	email               text,
 	cuit                text NOT NULL REFERENCES customer.person(cuit)
 );
+
+
+CREATE OR REPLACE FUNCTION customer.contact_information (
+	IN p_cuit           text,
+	IN p_address        text DEFAULT NULL,
+	IN p_phone          text DEFAULT NULL,
+	IN p_email          text DEFAULT NULL
+) RETURNS boolean AS $$
+DECLARE 
+
+BEGIN 
+	IF NOT EXISTS (SELECT 1 FROM customer.person WHERE cuit = p_cuit)
+	THEN 
+		RAISE WARNING 'ERROR: LA PERSONA NO EXISTE';
+		RETURN FALSE;
+	END IF;
+	
+	IF p_address IS NULL AND p_phone IS NULL AND p_email IS NULL 
+	THEN
+		RAISE WARNING 'ERROR: NINGUN DATO SUMINISTRADO';
+	ELSE 
+		INSERT INTO customer.contact_information (address, phone, mail, cuit)
+			VALUES(p_address, p_phone, p_mail, p_cuit);
+		
+		RETURN TRUE;
+	END IF;
+END;
+$$ LANGUAGE plpgsql
+SET search_path FROM CURRENT;
