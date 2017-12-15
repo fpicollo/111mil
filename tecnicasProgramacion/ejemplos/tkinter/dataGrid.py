e83e53ae1b49216330883737215f2537b0669821
German Basisty <basisty.german@gmail.com>
2017-12-15 15:27:05 -0300

    Actualizacion

--- tecnicasProgramacion/ejemplos/tkinter/dataGrid.py
+++ tecnicasProgramacion/ejemplos/tkinter/dataGrid.py
@@ -2,16 +2,28 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
 
 mainForm = tk.Tk()
 mainForm.title("Ejemplo Grid")
 mainForm.geometry("310x270")
 
-def borrarDatos():
-	dataGrid.delete(*dataGrid.get_children())
-
 def cargarDatos():
-	dataGrid.insert('', 'end', text = "Basisty", values=("German", 25969243))
+	persona = {}
+	
+	persona["apellido"] = sd.askstring("Datos Personales", "Ingrese Apellido")
+	persona["nombre"] = sd.askstring("Datos Personales", "Ingrese Nombre")
+	persona["telefono"] = sd.askstring("Datos Personales", "Ingrese Teléfono")
+	
+	agenda.append(persona)
+	
+	dataGrid.delete(*dataGrid.get_children())
+	
+	for persona in agenda:
+		dataGrid.insert('', 'end', text = persona["apellido"], values=(persona["nombre"], persona["telefono"]))
+
+agenda = []
 
 dataGrid = ttk.Treeview(mainForm)
 dataGrid["columns"]=("A", "B")
@@ -20,13 +32,10 @@ dataGrid.heading("#0", text='Apellido')
 dataGrid.column("#1", width=100)
 dataGrid.heading("#1", text="Nombre")
 dataGrid.column("#2", width=100)
-dataGrid.heading("#2", text="DNI")
+dataGrid.heading("#2", text="Teléfono")
 dataGrid.place(x = 5, y = 5)
 
-loadButton = tk.Button(mainForm, text = "Cargar Grid", command = cargarDatos)
-loadButton.place(x = 10, y = 230)
-
-loadButton = tk.Button(mainForm, text = "Borrar Grid", command = borrarDatos)
-loadButton.place(x = 140, y = 230)
+loadButton = tk.Button(mainForm, text = "Cargar Datos", command = cargarDatos)
+loadButton.place(x = 100, y = 230)
 
 mainForm.mainloop()
