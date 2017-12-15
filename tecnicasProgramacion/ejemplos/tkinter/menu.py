492253dca617a80177e69f159cc92c33cd39d4a3
German Basisty <basisty.german@gmail.com>
2017-12-15 14:23:47 -0300

    Actualizacion

--- tecnicasProgramacion/ejemplos/tkinter/menu.py
+++ tecnicasProgramacion/ejemplos/tkinter/menu.py
@@ -12,9 +12,14 @@ mainMenu = tk.Menu(mainForm)
 # Submenú de archivo
 fileMenu = tk.Menu(mainMenu, tearoff = 0)
 fileMenu.add_command(label = "Salir", command = quit) # le agrego un comando al menu (comando para salir)
-
-# Al "metamenu" principal le agrego en cascada el submenu de archivo
 mainMenu.add_cascade(label = "Archivo", menu = fileMenu)
+
+personMenu = tk.Menu(mainMenu, tearoff = 0)
+personMenu.add_command(label = "Agregar Persona")
+personMenu.add_separator()
+personMenu.add_command(label = "Mostrar Persona")
+mainMenu.add_cascade(label = "Persona", menu = personMenu)
+
 
 # Le digo a mainForm que su menu va a ser mainMenu
 mainForm.config(menu = mainMenu)
--- tecnicasProgramacion/ejemplos/tkinter/otro_formulario.py
+++ tecnicasProgramacion/ejemplos/tkinter/otro_formulario.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+from tkinter import messagebox as mb
 
-def otroFormulario():
+def otroFormulario(persona):
+	def guardarDatos(nombre, edad, persona):
+		persona["nombre"] = nombre
+		persona["edad"] = int(edad)
+
+		anotherForm.destroy()
+
 	anotherForm = tk.Toplevel(mainForm)
 	anotherForm.geometry("300x100")
 	anotherForm.title("Este es otro formulario")
 	
-	miLabel = tk.Label(anotherForm, text = "Este es otro formulario")
-	miLabel.place(x = 10, y = 10)
+	nombreLabel = tk.Label(anotherForm, text = "Nombre:")
+	nombreLabel.place(x = 10, y = 10)
+	nombreEntry = tk.Entry(anotherForm, width = 20)
+	nombreEntry.place(x = 10, y = 30)
 
+	edadLabel = tk.Label(anotherForm, text = "Edad:")
+	edadLabel.place (x = 10, y = 55)
+	edadEntry = tk.Entry(anotherForm, width = 5)
+	edadEntry.place(x = 10, y = 75)
+
+	miBoton = tk.Button(anotherForm, text = "Tomar Datos", command = \
+	  lambda: guardarDatos(nombreEntry.get(), edadEntry.get(), persona))
+	miBoton.place(x = 150, y = 70)
+
+persona = {}
 
 mainForm = tk.Tk()
 mainForm.geometry("300x100")
 mainForm.title("Ejemplo Otro Formulario")
 
-button = tk.Button(mainForm, text = "Abrir otro formulario", command = otroFormulario)
+button = tk.Button(mainForm, text = "Abrir otro formulario", command = lambda: otroFormulario(persona))
 button.place(x = 10, y = 10)
+
+button2 = tk.Button(mainForm, text = "Mostrar los datos", command = lambda: print(persona))
+button2.place(x = 10, y = 40)
 
 mainForm.mainloop()
