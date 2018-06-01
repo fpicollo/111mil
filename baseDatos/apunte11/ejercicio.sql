c70b376d6cb8008a7a6923732c674c6aa5d5c0c5
Germán C. Basisty <basisty.german@gmail.com>
2018-06-01 11:37:02 -0300

    Actualizacion

---
+++ baseDatos/apunte11/ejercicio.sql
@@ -0,0 +1,31 @@
+﻿create table person (
+	dni                  integer primary key,
+	name                 text not null
+);
+
+create table log (
+	id                   serial primary key,
+	timestamp            timestamp with time zone default now(),
+	detail               text not null
+);
+
+
+create or replace function person_insert_log_trg()
+returns trigger as $$
+begin
+	insert into log(detail) values('INSERTADO/A ' || NEW.name);
+	return NEW;
+END;
+$$ language plpgsql;
+
+create trigger person_insert_log_trg after insert on person for each row execute procedure person_insert_log_trg();
+
+create or replace function person_delete_log_trg()
+returns trigger as $$
+begin
+	insert into log(detail) values('ELIMINADO/A ' || OLD.name);
+	return OLD;
+END;
+$$ language plpgsql;
+
+create trigger person_delete_log_trg after delete on person for each row execute procedure person_delete_log_trg();
