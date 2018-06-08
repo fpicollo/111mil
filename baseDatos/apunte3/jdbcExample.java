9e6d02800a92010d37817a7501e33c44e6002eef
Germán C. Basisty <basisty.german@gmail.com>
2018-06-08 10:58:51 -0300

    Actualizacion

--- baseDatos/apunte3/jdbcExample.java
+++ baseDatos/apunte3/jdbcExample.java
@@ -5,46 +5,49 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 public class jdbcExample {
-    public static void main(String[] args) {
-        Connection dbConnection = null;
-        String url = "jdbc:postgresql://127.0.0.1:5432/prueba";
-        String username = "nombreDeUsuario";
-        String password = "1234";
-        
-        try {
-            Class.forName("org.postgresql.Driver");
-            dbConnection = DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e);
-        }
-        
-        String queryString = "INSERT INTO persona VALUES(13242424555, 'Juan Perez')";
-        Statement Query;
-        
-        try {
-            Query = dbConnection.createStatement();
-            Query.execute(queryString);
-            Query.close();
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e);
-        }
-        
-        
-        queryString = "SELECT * FROM persona";
-        ResultSet Result = null;
-        
-        try {
-            Query = dbConnection.createStatement();
-            Result = Query.executeQuery(queryString);
-            
-            while(Result.next()) {
-                System.out.print(Result.getString(1) + " | " + Result.getString(2));
-                System.out.println();
-            }
-            
-            Query.close();
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e);
-        }
-      }
+	public static void main(String[] args) {
+		Connection dbConnection = null;
+		
+		String url = "jdbc:postgresql://127.0.0.1:5432/prueba";
+		String username = "alumno";
+		String password = "1234";
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+			dbConnection = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e);
+		}
+		
+		String queryString = "INSERT INTO persona VALUES(12345678, 'Juan Perez')";
+		
+		Statement query = null;
+		
+		try {
+			query = dbConnection.createStatement();
+			query.execute(queryString);
+			query.close();
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e);
+		}
+		
+		
+		queryString = "SELECT * FROM persona";
+		ResultSet result = null;
+		
+		try {
+			query = dbConnection.createStatement();
+			result = query.executeQuery(queryString);
+			
+			while(result.next()) {
+				System.out.println(result.getString(1) + " | " + result.getString(2));
+			}
+			
+			query.close();
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e);
+		}
+		
+		try { dbConnection.close(); } catch(Exception e) {}
+	}
 }
